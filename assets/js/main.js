c9e9237a54d3da43117458bc583bfbcd499846b5
Abdullah Nahian <nahiansylhet@gmail.com>
2021-06-22 05:01:28 +0600

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,10 @@ $(document).ready(function () {
   });
 
   $('.counter').counterUp();
+
+  $('.testimonials').slick({
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-long-arrow-alt-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-long-arrow-alt-right"></i></button>',
+    dots: true,
+  });
 });
